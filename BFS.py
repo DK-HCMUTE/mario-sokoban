ea71381877bc84f2f0f890147ce2c86a05b8c729
MDung <dominhdung21082002@gmail.com>
2022-05-06 21:49:52 +0700

    bfs and GUI

--- BFS.py
+++ BFS.py
@@ -67,7 +67,7 @@ class Node:
         return next_node
 
     def solution(self):
-        return [node.action for node in self.path()[1:]]
+        return [node.state for node in self.path()[1:]]
 
     def path(self):
         node, path_back = self, []
--- main.py
+++ main.py
@@ -1,20 +1,82 @@
-#import pygame
+from turtle import Screen
+import pygame
 import numpy as np
+from sympy import carmichael
 from BFS import *
 from utilities import *
 
+maze = load_maze("1.txt")
+goal_state = load_goal_state("1.txt")
+SCREEN_SIZE = (600, 600)
+WIDTH = SCREEN_SIZE[0]/len(maze[0])
+HEIGHT = SCREEN_SIZE[0]/len(maze)
 
-# def game_zone():
-#     running = True
-#     while running:
-#         pass
+
+
+def load_game ():
+    global screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption ('SOKOBAN')
+    icon = pygame.image.load ('./assets/icon.jpg')
+    pygame.display.set_icon(icon)
+    
+    global floor
+    floor = pygame.image.load ('./assets/background.jpg')
+    floor = pygame.transform.scale(floor, (WIDTH, HEIGHT))
+    global wall
+    wall = pygame.image.load ('./assets/wall.jpg')
+    wall = pygame.transform.scale(wall,(WIDTH, HEIGHT))
+
+    global car
+    car = pygame.image.load ('./assets/car.jpg')
+    car = pygame.transform.scale(car, (WIDTH, HEIGHT))
+
+    global box
+    box = pygame.image.load ('./assets/box.jpg')
+    box = pygame.transform.scale(box, (WIDTH, HEIGHT))
+
+    global goal
+    goal = pygame.image.load ('./assets/goal.jpg')
+    goal = pygame.transform.scale(goal, (WIDTH, HEIGHT))
+
+
+def game_zone(problem):
+    pygame.init()
+    load_game()
+
+    running = True
+    res = BFS(problem)
+    board = res.solution()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # screen.blit(background, (0, 0))
+        pygame.display.update()
+        for k in board:
+            for i in range (len(k)):
+                for j in range (len(k[i])):
+                    if k[i][j] == "1":
+                        screen.blit (wall, (j*WIDTH, i*HEIGHT))
+                    elif k[i][j] == "0":
+                        screen.blit (floor, (j*WIDTH, i*HEIGHT))
+                    elif k[i][j] == "x":
+                        screen.blit (car, (j*WIDTH, i*HEIGHT))
+                    elif k[i][j] == "g":
+                        screen.blit (goal, (j*WIDTH, i*HEIGHT))
+                    elif k[i][j] == "*":
+                        screen.blit (box, (j*WIDTH, i*HEIGHT))
+        
+
 
 if __name__ == '__main__':
-    maze = load_maze("1.txt")
-    goal_state = load_goal_state("1.txt")
+    
     problem = SokobanProblem(maze,goal_state)
-    res = BFS(problem)
-    print(res.solution())
+    # print(res.solution())
+
+    game_zone(problem)
+    
 
 
 
